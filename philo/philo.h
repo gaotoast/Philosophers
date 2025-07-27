567ce2c53de00488fbe618175faa68819ebbed5d
shorei <160503699+gao0325ts@users.noreply.github.com>
2025-07-27 23:24:51 +0900

    philo.hを追加 - 使用可能関数のヘッダファイルをインクルード

---
+++ philo/philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stakada <stakada@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/27 23:20:29 by stakada           #+#    #+#             */
+/*   Updated: 2025/07/27 23:24:15 by stakada          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/time.h>
+# include <unistd.h>
+
+#endif
