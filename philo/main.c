dd9422e4e732cec56a25259c222a3dfe92ae6177
stakada <stakada@student.42tokyo.jp>
2025-07-30 18:30:41 +0900

    Fix: main - 処理成功時にも適切にリソースを解放

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: stakada <stakada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/11 17:13:45 by stakada           #+#    #+#             */
-/*   Updated: 2025/07/30 18:15:39 by stakada          ###   ########.fr       */
+/*   Updated: 2025/07/30 18:29:57 by stakada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,13 @@
 int	main(int argc, char **argv)
 {
 	t_data	data;
+	int		ret;
 
 	if (init(&data, argc, argv) < 0)
 		return (1);
-	if (simulate(&data) < 0)
-	{
-		free_data(&data);
+	ret = simulate(&data);
+	free_data(&data);
+	if (ret < 0)
 		return (1);
-	}
 	return (0);
 }
