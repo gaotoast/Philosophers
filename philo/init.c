5ebf60e119cee650474031a718cc50acff24cbe3
shorei <160503699+gao0325ts@users.noreply.github.com>
2025-08-06 17:41:09 +0900

    Norm: init.c

--- philo/init.c
+++ philo/init.c
@@ -6,7 +6,7 @@
 /*   By: stakada <stakada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/30 14:05:01 by stakada           #+#    #+#             */
-/*   Updated: 2025/08/06 16:41:47 by stakada          ###   ########.fr       */
+/*   Updated: 2025/08/06 17:40:52 by stakada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,31 +56,10 @@ static int	init_mutexes(t_data *data)
 	return (0);
 }
 
-static int	init_data(t_data *data, int argc, char **argv)
-{
-	if (parse_args(data, argc, argv) < 0)
-		return (-1);
-	data->forks = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t)
-			* data->n_of_philos);
-	if (!data->forks)
-		return (-1);
-	if (init_mutexes(data) < 0)
-		return (-1);
-	data->end_flag = 0;
-	data->current_turn = TURN_ODD;
-	data->odd_done = 0;
-	data->even_done = 0;
-	data->last_done = 0;
-	return (0);
-}
-
 static int	init_philos(t_data *data)
 {
 	int	i;
 
-	data->philos = (t_philo *)malloc(sizeof(t_philo) * data->n_of_philos);
-	if (!data->philos)
-		return (-1);
 	i = 0;
 	while (i < data->n_of_philos)
 	{
@@ -104,10 +83,33 @@ static int	init_philos(t_data *data)
 	return (0);
 }
 
+static int	init_data(t_data *data, int argc, char **argv)
+{
+	if (parse_args(data, argc, argv) < 0)
+		return (-1);
+	data->forks = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t)
+			* data->n_of_philos);
+	if (!data->forks)
+		return (-1);
+	if (init_mutexes(data) < 0)
+		return (-1);
+	data->philos = (t_philo *)malloc(sizeof(t_philo) * data->n_of_philos);
+	if (!data->philos)
+		return (-1);
+	if (init_philos(data) < 0)
+		return (-1);
+	data->end_flag = 0;
+	data->current_turn = TURN_ODD;
+	data->odd_done = 0;
+	data->even_done = 0;
+	data->last_done = 0;
+	return (0);
+}
+
 int	init(t_data *data, int argc, char **argv)
 {
 	memset(data, 0, sizeof(t_data));
-	if (init_data(data, argc, argv) < 0 || init_philos(data) < 0)
+	if (init_data(data, argc, argv) < 0)
 	{
 		clean_up_data(data);
 		return (-1);
